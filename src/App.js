01095ed7c9870b7e7c2ac0395ce3cd2de8e2fbfe
Marko Davidovic <davidovic-m@hotmail.com>
2023-11-20 00:55:51 -0500

    first connection

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+    // Make a GET request to the backend
+    axios.get('http://localhost:4000/api/data')
+      .then(response => {
+        setData(response.data.message);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{data}</h1>
     </div>
   );
 }
